5f4faa5ca5d1d0f576da7b7a7099569f70639415
Akash J Nair <akashjnair701@gmail.com>
2021-02-25 13:18:41 +0530

    Dice images are added

---
+++ android/app/src/main/kotlin/akashjnair/dice_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package akashjnair.dice_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(Dice());
+}
+
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.redAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Center(
+            child: Text(
+              "Dice",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("0"),
+                SizedBox(
+                  width: 100,
+                ),
+                Text("0"),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Image.asset('Dice/dice1.png'),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Image.asset('Dice/dice1.png'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
